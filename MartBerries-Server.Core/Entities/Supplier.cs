0fa62ec16f11df5486f633d332cf797f2a3a7a7a
lzy77_ <channelforletsplayshik@gmail.com>
2022-12-01 17:27:01 +0300

    Hotfix.

--- MartBerries-Server.Core/Entities/Supplier.cs
+++ MartBerries-Server.Core/Entities/Supplier.cs
@@ -11,5 +11,7 @@ namespace MartBerries_Server.Core.Entities
         public Guid Id { get; set; }
 
         public string Name { get; set; }
+
+        public virtual ICollection<SupplierProduct> Products { get; set; }
     }
 }
